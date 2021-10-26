280cd97c4e0ae5d2135f3d63b1721b04be4407bd
annvolfers <annvolfers@me.com>
2021-10-26 21:28:59 +0300

    fix css

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 const loginBtn = document.querySelector('.menu__login-btn');
 const loginCloseBtn = document.querySelector('.login__close');
+const loginBlock = document.querySelector('.login');
 const loginContainer = document.querySelector('.login__container');
 const registerBtn = document.querySelector('.menu__register-btn');
 const registerCloseBtn = document.querySelector('.register__close');
+const registerBlock = document.querySelector('.register');
 const registerContainer = document.querySelector('.register__container');
 
 loginBtn.addEventListener('click', openForm);
@@ -22,17 +24,17 @@ function openForm(event) {
 };
 
 function closeForm(event) {
-    if ((event.target === loginCloseBtn) || (!event.target.closest('.login'))) {
+    if (event.target === loginCloseBtn || !loginBlock.contains(event.target)) {
         loginContainer.classList.remove('visible');
-    } if ((event.target === registerCloseBtn) || (!event.target.closest('.register'))) {
+    } if (event.target === registerCloseBtn || !registerBlock.contains(event.target)) {
         registerContainer.classList.remove('visible');
     }
 };
 
 function closeFormEsc(event) {
-    if ((event.code == 'Escape') && (!loginContainer.classList.contains('non-visible'))) {
+    if (event.code == 'Escape' && loginContainer.classList.contains('visible')) {
         loginContainer.classList.remove('visible');
-    } if ((event.code == 'Escape') && (!registerContainer.classList.contains('non-visible'))) {
+    } if (event.code == 'Escape' && registerContainer.classList.contains('visible')) {
         registerContainer.classList.remove('visible');
     }
 }
